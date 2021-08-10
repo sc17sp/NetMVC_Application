010203da50269d5cf494d4590171333071dd5cb4
sc17sp <sean.parker@x-labsystems.co.uk>
2021-08-10 11:41:56 +0100

    Added static list of students to populate Index

--- TrainingMVCApp/TrainingMVCApp/Controllers/StudentController.cs
+++ TrainingMVCApp/TrainingMVCApp/Controllers/StudentController.cs
@@ -3,11 +3,22 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using TrainingMVCApp.Models;
 
 namespace TrainingMVCApp.Controllers
 {
     public class StudentController : Controller
     {
+        static IList<Student> studentList = new List<Student>
+        {
+            new Student() { StudentID = 1, StudentName = "John", Age = 18 } ,
+            new Student() { StudentID = 2, StudentName = "Steve",  Age = 21 } ,
+            new Student() { StudentID = 3, StudentName = "Bill",  Age = 25 } ,
+            new Student() { StudentID = 4, StudentName = "Ram" , Age = 20 } ,
+            new Student() { StudentID = 5, StudentName = "Ron" , Age = 31 } ,
+            new Student() { StudentID = 4, StudentName = "Chris" , Age = 17 } ,
+            new Student() { StudentID = 4, StudentName = "Rob" , Age = 19 }
+        };
         // GET: Student
         public ActionResult Index()
         {
