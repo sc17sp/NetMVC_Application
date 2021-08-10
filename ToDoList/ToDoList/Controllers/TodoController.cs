ce71891d4ec0a565e2853bdda93b400f1944280f
sc17sp <sean.parker@x-labsystems.co.uk>
2021-08-10 15:28:41 +0100

    static list displated of ToDo items

--- ToDoList/ToDoList/Controllers/TodoController.cs
+++ ToDoList/ToDoList/Controllers/TodoController.cs
@@ -3,16 +3,24 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using ToDoList.Models;
 
 namespace ToDoList.Controllers
 {
     public class TodoController : Controller
     {
+        static IList<Todo> todoList = new List<Todo> 
+        {
+            new Todo() { TodoId = 1, Message = "Go do some shopping"},
+            new Todo() { TodoId = 2, Message = "Call mum"},
+            new Todo() { TodoId = 3, Message = "Learn some SQL"},
+            new Todo() { TodoId = 4, Message = "Learn to fly"}
+        };
         // GET: Todo
         public ActionResult Index()
         {
-
-            return View();
+        
+            return View(todoList);
         }
 
         public ActionResult Add() {
