0e363fddfa0695a62775ee2b8ba1975e57b1aa7f
sc17sp <sean.parker@x-labsystems.co.uk>
2021-08-10 11:12:14 +0100

    Added Route for Student

--- TrainingMVCApp/TrainingMVCApp/App_Start/RouteConfig.cs
+++ TrainingMVCApp/TrainingMVCApp/App_Start/RouteConfig.cs
@@ -14,6 +14,12 @@ namespace TrainingMVCApp
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
 
             routes.MapRoute(
+                name: "Student",
+                url: "students/{id}",
+                defaults: new { controller = "Student", action = "Index"}
+                );
+
+            routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
                 defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
