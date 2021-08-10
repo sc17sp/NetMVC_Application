faf4f57a81058806ee235a87514483c31b1d28ff
sc17sp <sean.parker@x-labsystems.co.uk>
2021-08-10 15:19:35 +0100

    Addded Todo model

---
+++ ToDoList/ToDoList/Models/Todo.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace ToDoList.Models
+{
+    public class Todo
+    {
+        public int TodoId { get; set; }
+        [Required]
+        public string Message { get; set; }
+    }
+}
